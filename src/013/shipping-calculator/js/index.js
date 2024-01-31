31efe97b14bfc7776846ac12bd1e25c9c458a042
swagylicious <jflores23@nait.ca>
2024-01-31 17:24:05 +0000

    Jan 31, 2024, 10:24 AM

--- src/013/shipping-calculator/js/index.js
+++ src/013/shipping-calculator/js/index.js
@@ -7,6 +7,7 @@ const form = document.querySelector('#shipping-calculator')
 const calculateQuote = function(ev){
     ev.preventDefault()
     log('form submitted')
+    const/
 }
 
 form.addEventListener('submit', calculateQuote)
