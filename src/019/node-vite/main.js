35330b0e37526b297bc088c40a7fb5d99edd5bf0
swagylicious <jflores23@nait.ca>
2024-03-06 17:41:15 +0000

    Mar 6, 2024, 10:41 AM

--- src/019/node-vite/main.js
+++ src/019/node-vite/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import { demoFetch } from 
 
 document.querySelector('#app').innerHTML = `
   <div>
