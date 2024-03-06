8f13d595c92bfd1cbed26fa3d00bcc0f89c3a5d8
swagylicious <jflores23@nait.ca>
2024-03-06 17:50:12 +0000

    Mar 6, 2024, 10:50 AM

--- src/019/node-vite/demo.js
+++ src/019/node-vite/demo.js
@@ -1,5 +1,6 @@
 const url = 'https://stapi.co/v2/rest/spacecraft/search';
 const demoFetch = function (){
+    const 
     fetch(url)
         .then(response => response.json())
         .then(data => console.log(data))
