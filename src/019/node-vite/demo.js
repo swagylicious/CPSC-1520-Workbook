7155bd20da2b38eb4b67b6d90e03b6120dfdd5ef
swagylicious <jflores23@nait.ca>
2024-03-06 18:23:49 +0000

    Mar 6, 2024, 11:23 AM

--- src/019/node-vite/demo.js
+++ src/019/node-vite/demo.js
@@ -4,7 +4,7 @@ const dumpInfo = function (element, obj) {
     console.log(element);
     console.log(obj);
     if (element && obj) {
-        let content = '<table><tr><th></tr><tr><th><><>';
+        let content = '<table><tr><th>Name</th></tr><tr><th></th><>';
     }
     else {
         console.log('element or obj is not defined');
